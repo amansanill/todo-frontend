1bc796253626c3377ba510ad90f86d9996faaf69
Aman Sanil <aman.a.sanil@gmail.com>
2025-03-20 15:56:55 +0530

    env updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"; // Change to your backend URL
+const API_URL = process.env.REACT_APP_API_URL || "http://13.201.80.140:5000"; // Change to your backend URL
 
 function App() {
   const [todos, setTodos] = useState([]);
